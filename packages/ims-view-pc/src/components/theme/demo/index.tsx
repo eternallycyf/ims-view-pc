3463a1e32a5be6ce69409b65562c1a5f4b39748e
eternallycyf <969475322@qq.com>
2023-11-18 13:20:18 +0800

    ✨ feat: add custom theme docs

---
+++ packages/ims-view-pc/src/components/theme/demo/index.tsx
@@ -0,0 +1,69 @@
+import { ConfigProvider, Form, InputNumber, Space } from 'antd';
+import { AccessBtn } from 'ims-view-pc';
+import React, { useState } from 'react';
+
+export const ACTIVE_TYPE = [
+  { label: '满折', value: '0' },
+  { label: '满减', value: '1' },
+  { label: '立减', value: '2' },
+] as const;
+
+const App: React.FC = () => {
+  const [groupValue, getGroupValue] = useState<(typeof ACTIVE_TYPE)[number]['value']>('1');
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          // Seed Token，影响范围大
+          colorPrimary: '#00b96b',
+          borderRadius: 2,
+
+          // 派生变量，影响范围小
+          colorBgContainer: '#f6ffed',
+        },
+      }}
+    >
+      <AccessBtn
+        btnList={[
+          {
+            type: 'default',
+            buttonType: 'link',
+            element: '链接',
+            itemProps: {
+              buttonProps: {
+                onClick: (e) => console.log(e),
+              },
+            },
+          },
+          {
+            type: 'custom',
+            element: (
+              <Form.Item label="万" style={{ marginBottom: 0 }}>
+                <InputNumber />
+              </Form.Item>
+            ),
+          },
+          {
+            type: 'delete',
+            element: '删除',
+            itemProps: {
+              deleteText: '确认删除嘛?',
+              handleDeleteConfirm: (e) => console.log(e),
+            },
+          },
+          {
+            type: 'group',
+            itemProps: {
+              groupValue,
+              handleGroupValueOnChange: (val: (typeof ACTIVE_TYPE)[number]['value']) =>
+                getGroupValue(val),
+              groupDict: ACTIVE_TYPE,
+            },
+          },
+        ]}
+      />
+    </ConfigProvider>
+  );
+};
+
+export default App;
---
+++ packages/ims-view-pc/src/components/theme/index.md
@@ -0,0 +1,15 @@
+---
+title: 定制主题
+order: 0
+nav:
+  title: 组件
+  order: 1
+apiHeader: false
+---
+
+## 简述
+
+- 与`antd v5`主题配置方法保持统一, 只需要使用 `ConfigProvider` 组件包裹即可
+- 具体可参考 antd [主题配置方法](https://ant-design.antgroup.com/docs/react/customize-theme-cn)
+
+<code src="./demo/index.tsx">主题</code>
