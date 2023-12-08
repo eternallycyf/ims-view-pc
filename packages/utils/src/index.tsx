73bdb86d7c5a48cd2c3ab9cea3db3bdbc526c499
eternallycyf <969475322@qq.com>
2023-12-09 02:05:12 +0800

    ✨ feat(@ims-view/utils): remove ase

--- packages/utils/src/index.tsx
+++ packages/utils/src/index.tsx
@@ -1,5 +1,4 @@
 export { default as applyAwait } from './applyAwait';
-export { default as ase } from './ase';
 export { default as decimal } from './decimal';
 export { default as mergeProps } from './mergeProps';
 export { createPubSub, default as PubSub } from './PubSub';
