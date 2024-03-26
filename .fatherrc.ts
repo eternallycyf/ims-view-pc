6b60819e21e037c36ed7fe62851501f158cde6b8
eternallycyf <969475322@qq.com>
2024-03-26 09:11:48 +0800

    ✨ feat: ✨ feat: add es build

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'father';
 
 export default defineConfig({
   cjs: { output: 'lib', platform: 'browser' },
+  esm: { output: 'es' },
   extraBabelPlugins: ['add-module-exports'],
 });
