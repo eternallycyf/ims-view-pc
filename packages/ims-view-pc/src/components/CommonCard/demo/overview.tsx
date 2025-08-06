378659b0859ccfc64a0a16b38cd7db740c482eb2
eternallycyf <969475322@qq.com>
2025-08-06 19:45:21 +0800

    ✨ feat(ims-view-pc): update card demo

--- packages/ims-view-pc/src/components/CommonCard/demo/overview.tsx
+++ packages/ims-view-pc/src/components/CommonCard/demo/overview.tsx
@@ -1,37 +1,60 @@
 import { CommonCard } from 'ims-view-pc';
 import React from 'react';
 
+const enum AnchorEnum {
+  FIRST = '1',
+  SECOND = '2',
+  THIRD = '3',
+  FOURTH = '4',
+  FIFTH = '5',
+}
+
 const AnchorList = [
-  { label: '1', value: '1' },
-  { label: '2', value: '2' },
-  { label: '3', value: '3' },
-  { label: '4', value: '4' },
-  { label: '5', value: '5' },
+  {
+    label: '锚点1',
+    value: AnchorEnum.FIRST,
+  },
+  {
+    label: '锚点2',
+    value: AnchorEnum.SECOND,
+  },
+  {
+    label: '锚点3',
+    value: AnchorEnum.THIRD,
+  },
+  {
+    label: '锚点4',
+    value: AnchorEnum.FOURTH,
+  },
+  {
+    label: '锚点5',
+    value: AnchorEnum.FIFTH,
+  },
 ] as const;
 
-type IAnchorId = (typeof AnchorList)[number]['value'];
+type IAnchorId = `${AnchorEnum}`;
 
 const Overview = (props) => {
   return (
     <CommonCard.AnchorCard<IAnchorId> id="detail" btnList={AnchorList} rightChildren="右侧内容">
       <div style={{ height: 400 }}>
-        <CommonCard.AnchorLink<IAnchorId> id="1" />1
+        <CommonCard.AnchorLink<IAnchorId> id={AnchorEnum.FIRST} />1
         <CommonCard.Line />
       </div>
       <div style={{ height: 400 }}>
-        <CommonCard.AnchorLink<IAnchorId> id="2" />2
+        <CommonCard.AnchorLink<IAnchorId> id={AnchorEnum.SECOND} />2
         <CommonCard.Line />
       </div>
       <div style={{ height: 400 }}>
-        <CommonCard.AnchorLink<IAnchorId> id="3" />3
+        <CommonCard.AnchorLink<IAnchorId> id={AnchorEnum.THIRD} />3
         <CommonCard.Line />
       </div>
       <div style={{ height: 400 }}>
-        <CommonCard.AnchorLink<IAnchorId> id="4" />4
+        <CommonCard.AnchorLink<IAnchorId> id={AnchorEnum.FOURTH} />4
         <CommonCard.Line />
       </div>
       <div style={{ height: 400 }}>
-        <CommonCard.AnchorLink<IAnchorId> id="5" />5
+        <CommonCard.AnchorLink<IAnchorId> id={AnchorEnum.FIFTH} />5
         <CommonCard.Line />
       </div>
     </CommonCard.AnchorCard>
