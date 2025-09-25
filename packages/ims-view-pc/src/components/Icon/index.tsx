ee9ec4431f01ab14aa3aed5883fdaedd2c94c065
eternallycyf <969475322@qq.com>
2025-09-26 01:09:18 +0800

    🐛 fix(ims-view-pc): iconfont build error in esm

--- packages/ims-view-pc/src/components/Icon/index.tsx
+++ packages/ims-view-pc/src/components/Icon/index.tsx
@@ -1,18 +1,32 @@
 import { createFromIconfontCN } from '@ant-design/icons';
 import { FC } from 'react';
-import iconfont from '../../styles/iconfont/iconfont.js';
 import { iconFontJSON } from './iconfont';
 
 type IconType = (typeof iconFontJSON)['glyphs'][number]['font_class'];
 type CapitalizeStr<Str extends string> = Str extends `${infer Rest}` ? `icon-${Rest}` : Str;
+
 export interface IconProps extends React.HTMLProps<HTMLSpanElement> {
   type: CapitalizeStr<IconType>;
   spin?: boolean;
   rotate?: number;
 }
 
+import localIconfont from '../../styles/iconfont/iconfont.js';
+
+function getIconScriptUrl() {
+  if (typeof window === 'undefined') return '';
+  const script = document.createElement('script');
+  script.src = localIconfont; // 尝试本地
+  script.onerror = () => {
+    script.src = 'https://ims-view-pc-eternallycyfs-projects.vercel.app/iconfont/iconfont.js'; // 在线回退
+    document.body.appendChild(script);
+  };
+  document.body.appendChild(script);
+  return localIconfont; // 先返回本地路径，createFromIconfontCN 会用这个
+}
+
 const Icon: FC<IconProps> = createFromIconfontCN({
-  scriptUrl: iconfont,
+  scriptUrl: getIconScriptUrl(),
 });
 
 export default Icon;
